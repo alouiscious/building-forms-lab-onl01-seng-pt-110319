3a17c23c8dab146a0772cedaed426edb3f6f0226
Alouiscious Media <info@reneesoul.com>
2020-09-18 18:56:12 -0700

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,35 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleOnChange = event => {
+    this.setState ({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleOnsubmit = event => {
+    event.preventDefault()
+    const bandProfile = {...this.state}
+    this.props.addBand(bandProfile)
+    this.setState({
+      name: ''
+    })
+  }
+
+
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleOnsubmit}>
+          <input type="text" onChange={this.handleOnChange} placeholder="Enter a Band Name" />
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
